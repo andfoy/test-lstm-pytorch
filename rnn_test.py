85e3cd5db81a84b0755728b7cf5b0f70ac203712
Edgar Andrés Margffoy Tuay <andfoy@gmail.com>
2020-03-09 23:28:58 -0500

    Update param

--- rnn_test.py
+++ rnn_test.py
@@ -1,7 +1,7 @@
 import torch
 import torch.nn as nn
 
-lstm = nn.LSTM(256, 128, bidirectional=True, layers=3,
+lstm = nn.LSTM(256, 128, bidirectional=True, num_layers=3,
                cat_layer_fwd_bwd_states=True)
 
 x = torch.rand(10, 1, 256)
